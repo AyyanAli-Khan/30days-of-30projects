268779560f7a20bcaae999f01f72d093eb8290c9
Ayyan Ali Khan <ayyanalikhan510@gmail.com>
2024-09-01 20:08:46 +0500

    setup

---
+++ setup-Day 0/app/page.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <main className="w-full h-screen gap-10 bg-zinc-900 flex items-center justify-center flex-col">
+      <Button>Home page</Button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
